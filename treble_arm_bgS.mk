3de4c1eab585fc6823ce59299a4c7b36bf091c6a
YYY2333333 <67032996+YYY2333333@users.noreply.github.com>
2020-07-29 21:23:16 +0800

    Add:arm/arm64,a/ab.mk

---
+++ treble_arm_bgS.mk
@@ -0,0 +1,16 @@
+TARGET_GAPPS_ARCH := arm
+$(call inherit-product, device/phh/treble/base-pre.mk)
+include build/make/target/product/aosp_arm_ab.mk
+$(call inherit-product, vendor/vndk/vndk-binder32.mk)
+$(call inherit-product, device/phh/treble/base.mk)
+
+$(call inherit-product, device/phh/treble/gapps.mk)
+$(call inherit-product, device/phh/treble/lineage.mk)
+
+PRODUCT_NAME := treble_arm_bgS
+PRODUCT_DEVICE := phhgsi_arm_ab
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Phh-Treble with GApps
+
+PRODUCT_PACKAGES +=  phh-su me.phh.superuser
+
